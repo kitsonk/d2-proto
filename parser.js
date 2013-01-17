3c823461b4c6570c36a5e9ae629ccfcca7a096b1
Kitson Kelly <dojo@kitsonkelly.com>
2013-01-17 13:24:51 +0000

    add declarative scripting, add code comments

--- parser.js
+++ parser.js
@@ -1,19 +1,29 @@
 define([
 	"require",
 	"./debug", // debug.warn
-	"dojo/_base/lang", // lang.getObject, lang.mixin
+	"dojo/_base/lang", // lang.getObject, lang.setObject, lang.mixin, lang.hitch
 	"dojo/_base/window", // win.document
+	"dojo/aspect", // aspect.before, aspect.around, aspect.after
 	"dojo/Deferred",
+	"dojo/on",
 	"dojo/query",
 	"dojo/domReady!"
-], function(require, debug, lang, win, Deferred, query) {
+], function(require, debug, lang, win, aspect, Deferred, on, query) {
 
-	var typeAttribute = "data-dojo-type",
-		propsAttribute = "data-dojo-props",
-		mixinsAttribute = "data-dojo-mixins",
-		jsIdAttribute = "data-dojo-id",
-		attachPointAttribute = "data-dojo-attach-point",
-		attachEventAttribute = "data-dojo-attach-event";
+	var scopeBase = "dojo",
+		attributeBase = "data-" + scopeBase + "-",
+		typeAttribute = attributeBase + "type",
+		propsAttribute = attributeBase + "props",
+		mixinsAttribute = attributeBase + "mixins",
+		jsIdAttribute = attributeBase + "id",
+		attachPointAttribute = attributeBase + "attach-point",
+		attachEventAttribute = attributeBase + "attach-event",
+		scriptArgsAttribute = attributeBase + "args",
+		scriptWithAttribute = attributeBase + "with",
+		scriptEventAttribute = attributeBase + "event",
+		scriptPropAttribute = attributeBase + "prop",
+		scriptMethodAttribute = attributeBase + "method",
+		scriptTypeBase = scopeBase + "/";
 
 	var ctorMap = {};
 	function getCtor(types) {
@@ -63,6 +73,29 @@ define([
 			return dfd.promise;
 	}
 
+	function functionFromScript(script){
+		// summary:
+		//		Take a `<script>` DOMNode and convert it into a Function.
+		// script: DOMNode
+		//		The DOMNode to convert into a Function.  If the node contains an argument or with attribute, those are
+		//		added to the returned function.
+		// returns: Function
+		var prefix = "",
+			suffix = "",
+			args = script.getAttribute(scriptArgsAttribute) || "",
+			w = script.getAttribute(scriptWithAttribute);
+
+		if(w){
+			w.split(/\s*,\s*/).forEach(function(part) {
+				prefix += "with(" + part + ") {";
+				suffix += "}";
+			});
+		}
+
+		return new Function(args ? args.split(/\s*,\s*/) : [], prefix + script.innerHTML + suffix);
+
+	}
+
 	function convertPropsString(props){
 		// summary:
 		//		eval is evil, except when it isn't.  There is no other way to take a text string and convert it into a
@@ -82,18 +115,97 @@ define([
 			//	|		ctor: function() {} // Optional constructor function to use or ``null``
 			//	|	}
 			// options: Object?
-			//		A hash of options.  See .parse() for details
+			//		A hash of options.  See .parse() for details.
 			// returns: Array
 
 			options = options || {};
 
 			var instances = objects.map(function(obj){
-				if(!obj.ctor){
+				if(!obj.ctor) {
 					obj.ctor = getCtor(obj.types);
+					obj.proto = obj.ctor && obj.ctor.prototype;
 				}
 				var props = obj.node.getAttribute(propsAttribute);
 				if(props) props = convertPropsString(props);
+
+				var scripts = [];
+				if(!(obj.ctor && obj.ctor._noScript || obj.proto._noScript)) {
+					query('> script[type^="dojo/"]', obj.node).forEach(function(script) {
+						obj.node.removeChild(script);
+						var scriptType = script.getAttribute("type");
+						switch(scriptType){
+							case scriptTypeBase + "on":
+								scripts.push({
+									node: script,
+									type: "on",
+									evt: script.getAttribute(scriptEventAttribute)
+								});
+								break;
+							case scriptTypeBase + "watch":
+								scripts.push({
+									node: script,
+									type: "watch",
+									prop: script.getAttribute(scriptPropAttribute)
+								});
+								break;
+							case scriptTypeBase + "before":
+								scripts.push({
+									node: script,
+									type: "before",
+									method: script.getAttribute(scriptMethodAttribute)
+								});
+								break;
+							case scriptTypeBase + "around":
+								scripts.push({
+									node: script,
+									type: "around",
+									method: script.getAttribute(scriptMethodAttribute)
+								});
+								break;
+							case scriptTypeBase + "after":
+								scripts.push({
+									node: script,
+									type: "after",
+									method: script.getAttribute(scriptMethodAttribute)
+								});
+								break;
+							default:
+								scripts.push({
+									node: script,
+									type: "default"
+								});
+						}
+					});
+				}
+
 				var instance = new obj.ctor(props, obj.node);
+
+				// Add the instance to the global scope if jsID attribute is set
+				var jsId = obj.node.getAttribute(jsIdAttribute);
+				if(jsId){
+					lang.setObject(jsId, instance);
+				}
+
+				// Loop through the scripts, post the instantiation of the object and set them up
+				scripts.forEach(function(script){
+					var fn = functionFromScript(script.node);
+					switch(script.type){
+						case "on":
+							on(instance, script.evt, fn);
+							break;
+						case "watch":
+							instance.watch(script.prop, fn);
+							break;
+						case "before":
+						case "around":
+						case "after":
+							aspect[script.type](instance, script.method, lang.hitch(instance, fn), true);
+							break;
+						default:
+							fn.call(instance);
+					}
+				});
+
 				return instance;
 			});
 
@@ -109,6 +221,16 @@ define([
 		},
 
 		scan: function(rootNode, options) {
+			// summary:
+			//		Scan the DOM for decorated nodes that should be instantiated as Objects.  It returns a promise that
+			//		is fulfilled with an array of Objects that contain information about how the nodes should be
+			//		instantiated.
+			// rootNode: DOMNode?
+			//		The DOMNode that should serve as the base for the scan.  If null or undefined, defaults to the
+			//		body of the window.
+			// options: Object?
+			//		A hash of options.  See .parse() for details.
+			// returns: dojo/promise/Promise
 			rootNode = rootNode || win.body();
 			options = options || {};
 
@@ -146,6 +268,21 @@ define([
 		},
 
 		parse: function(rootNode, options) {
+			// summary:
+			//		Scan the DOM for decorated nodes that should be instantiated as Object and instantiate the objects.
+			//		The return is a promise which is fulfilled with an array of the instantiated objects.
+			// rootNode: DOMNode?
+			//		The DOMNode that should serve as the base for the scan.  If null or undefined, defaults to the
+			//		body of the window.
+			// options: Object?
+			//		A hash of options:
+			//
+			//			 Option | Default | Description
+			//			--------|---------|-------------
+			//			noStart | `false` | Whether or not to call `.startup()` on instances.
+			//
+			// returns: dojo/promise/Promise
+
 			rootNode = rootNode || win.body();
 			options = options || {};
 

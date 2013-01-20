60f915dfaf4ebb3c94a5f01b95c5d70e7a249591
Kitson Kelly <dojo@kitsonkelly.com>
2013-01-20 10:13:36 +0000

    full linting in in line with dojo2-core

--- debug.js
+++ debug.js
@@ -1,15 +1,18 @@
 define([
-], function(){
+	'dojo/has'
+], function (has) {
 	// This is just a stub module for what will hopefully be a more robust module in Dojo 2.0
 
 	return {
-		log: function() {
+		log: function () {
 			console.log.apply(console, arguments);
 		},
-		warn: function() {
-			console.warn.apply(console, arguments);
+		warn: function () {
+			if (has('config-isDebug')) {
+				console.warn.apply(console, arguments);
+			}
 		},
-		error: function() {
+		error: function () {
 			console.error.apply(console, arguments);
 		}
 	};
--- parser.js
+++ parser.js
@@ -1,31 +1,34 @@
 define([
-	"require",
-	"./debug", // debug.warn
-	"dojo/_base/lang", // lang.getObject, lang.setObject, lang.mixin, lang.hitch
-	"dojo/_base/window", // win.document
-	"dojo/aspect", // aspect.before, aspect.around, aspect.after
-	"dojo/Deferred", // Deferred
-	"dojo/on", // on
-	"dojo/promise/all", // all
-	"dojo/query", // query().forEach
-	"dojo/when", // when
-	"dojo/domReady!"
-], function(require, debug, lang, win, aspect, Deferred, on, all, query, when) {
+	'require',
+	'./debug', // debug.warn
+	'dojo/_base/lang', // lang.getObject, lang.setObject, lang.mixin, lang.hitch
+	'dojo/_base/window', // win.document
+	'dojo/aspect', // aspect.before, aspect.around, aspect.after
+	'dojo/Deferred', // Deferred
+	'dojo/on', // on
+	'dojo/promise/all', // all
+	'dojo/query', // query().forEach
+	'dojo/when', // when
+	'dojo/domReady!'
+], function (require, debug, lang, win, aspect, Deferred, on, all, query, when) {
 
-	var scopeBase = "dojo",
-		attributeBase = "data-" + scopeBase + "-",
-		typeAttribute = attributeBase + "type",
-		propsAttribute = attributeBase + "props",
-		mixinsAttribute = attributeBase + "mixins",
-		jsIdAttribute = attributeBase + "id",
-		attachPointAttribute = attributeBase + "attach-point",
-		attachEventAttribute = attributeBase + "attach-event",
-		scriptArgsAttribute = attributeBase + "args",
-		scriptWithAttribute = attributeBase + "with",
-		scriptEventAttribute = attributeBase + "event",
-		scriptPropAttribute = attributeBase + "prop",
-		scriptMethodAttribute = attributeBase + "method",
-		scriptTypeBase = scopeBase + "/";
+	// eval is evil, except when it isn't, and it isn't in the parser
+	/*jshint evil:true */
+
+	var scopeBase = 'dojo',
+		attributeBase = 'data-' + scopeBase + '-',
+		typeAttribute = attributeBase + 'type',
+		propsAttribute = attributeBase + 'props',
+		mixinsAttribute = attributeBase + 'mixins',
+		jsIdAttribute = attributeBase + 'id',
+		attachPointAttribute = attributeBase + 'attach-point',
+		attachEventAttribute = attributeBase + 'attach-event',
+		scriptArgsAttribute = attributeBase + 'args',
+		scriptWithAttribute = attributeBase + 'with',
+		scriptEventAttribute = attributeBase + 'event',
+		scriptPropAttribute = attributeBase + 'prop',
+		scriptMethodAttribute = attributeBase + 'method',
+		scriptTypeBase = scopeBase + '/';
 
 	var ctorMap = {};
 	function getCtor(types) {
@@ -40,9 +43,9 @@ define([
 		// returns: Object
 		//		An object that is intended to be a constructor prototype to be instantiated.
 		var ts = types.join();
-		if(!ctorMap[ts]) {
-			var mixins = types.map(function(t){
-				return ctorMap[t] = ctorMap[t] || (~t.indexOf("/") && require(t)) || lang.getObject(t);
+		if (!ctorMap[ts]) {
+			var mixins = types.map(function (t) {
+				return ctorMap[t] = ctorMap[t] || (~t.indexOf('/') && require(t)) || lang.getObject(t);
 			});
 			var ctor = mixins.shift();
 			ctorMap[ts] = mixins.length ? (ctor.createSubclass ? ctor.createSubclass(mixins) :
@@ -60,13 +63,13 @@ define([
 		// returns: dojo/promise/Promise
 
 		var dfd = new Deferred();
-		if(mids && mids.length){
-			debug.warn("WARNING: Auto-requiring modules");
+		if (mids && mids.length) {
+			debug.warn('WARNING: Auto-requiring modules: ' + mids.join(', '));
 			try {
-				require(mids, function(){
+				require(mids, function () {
 					dfd.resolve(Array.prototype.slice.call(arguments, 0));
 				});
-			} catch(e) {
+			} catch (e) {
 				dfd.reject(e);
 			}
 		} else {
@@ -80,26 +83,26 @@ define([
 		//		Takes the `innerHTML` of a DOMNode, converts it into a object hash, requires in the supplied modules
 		//		based on their module ID and then sets the modules as global objects as identified by the key in their
 		//		hash.
-		// 
+		//
 		var dfd = new Deferred();
 
-		var midHash = eval("({" + node.innerHTML + "})"),
+		var midHash = eval('({' + node.innerHTML + '})'),
 			vars = [],
 			mids = [];
 
-		for(var name in midHash) {
+		for (var name in midHash) {
 			vars.push(name);
 			mids.push(midHash[name]);
 		}
 
 		try {
-			require(mids, function() {
-				vars.forEach(function(name, idx) {
+			require(mids, function () {
+				vars.forEach(function (name, idx) {
 					lang.setObject(name, arguments[idx]);
 				});
 				dfd.resolve(arguments);
 			});
-		} catch(e) {
+		} catch (e) {
 			dfd.reject(e);
 		}
 
@@ -113,15 +116,15 @@ define([
 		//		The DOMNode to convert into a Function.  If the node contains an argument or with attribute, those are
 		//		added to the returned function.
 		// returns: Function
-		var prefix = "",
-			suffix = "",
-			args = script.getAttribute(scriptArgsAttribute) || "",
+		var prefix = '',
+			suffix = '',
+			args = script.getAttribute(scriptArgsAttribute) || '',
 			w = script.getAttribute(scriptWithAttribute);
 
-		if(w){
-			w.split(/\s*,\s*/).forEach(function(part) {
-				prefix += "with(" + part + ") {";
-				suffix += "}";
+		if (w) {
+			w.split(/\s*,\s*/).forEach(function (part) {
+				prefix += 'with(' + part + ') {';
+				suffix += '}';
 			});
 		}
 
@@ -129,22 +132,22 @@ define([
 
 	}
 
-	function convertPropsString(props){
+	function convertPropsString(props) {
 		// summary:
 		//		eval is evil, except when it isn't.  There is no other way to take a text string and convert it into a
 		//		JavaScript object.
-		return eval("({" + props + "})");
+		return eval('({' + props + '})');
 	}
 
 	return {
-		instantiate: function(objects, options) {
+		instantiate: function (objects, options) {
 			// summary:
 			//		Instantiate an array of objects and return the instantiated instances
 			// objects: Array
 			//		An array of objects to instantiate, where are objects are a hash in the format of:
 			//	|	{
 			//	|		node: DOMNode, // A DOM node that is associated with the Object
-			//	|		types: ["module/id", "mixin/module"], // A string array of modules to derive a constructor
+			//	|		types: ['module/id', 'mixin/module'], // A string array of modules to derive a constructor
 			//	|		ctor: function() {} // Optional constructor function to use or ``null``
 			//	|	}
 			// options: Object?
@@ -153,61 +156,63 @@ define([
 
 			options = options || {};
 
-			var instances = objects.map(function(obj){
-				if(!obj.ctor) {
+			var instances = objects.map(function (obj) {
+				if (!obj.ctor) {
 					obj.ctor = getCtor(obj.types);  // Get ctor will now throw if it cannot be resolved
 					obj.proto = obj.ctor && obj.ctor.prototype;
 				}
 				var props = obj.node.getAttribute(propsAttribute);
-				if(props) props = convertPropsString(props);
+				if (props) {
+					props = convertPropsString(props);
+				}
 				props = props || {};
 
 				var scripts = [];
-				if(!((obj.ctor && obj.ctor._noScript) || (obj.proto && obj.proto._noScript))) {
-					query('> script[type^="dojo/"]', obj.node).forEach(function(script) {
+				if (!((obj.ctor && obj.ctor._noScript) || (obj.proto && obj.proto._noScript))) {
+					query('> script[type^="dojo/"]', obj.node).forEach(function (script) {
 						obj.node.removeChild(script);
-						var scriptType = script.getAttribute("type");
-						switch(scriptType){
-							case scriptTypeBase + "on":
-								scripts.push({
-									node: script,
-									type: "on",
-									evt: script.getAttribute(scriptEventAttribute)
-								});
-								break;
-							case scriptTypeBase + "watch":
-								scripts.push({
-									node: script,
-									type: "watch",
-									prop: script.getAttribute(scriptPropAttribute)
-								});
-								break;
-							case scriptTypeBase + "before":
-								scripts.push({
-									node: script,
-									type: "before",
-									method: script.getAttribute(scriptMethodAttribute)
-								});
-								break;
-							case scriptTypeBase + "around":
-								scripts.push({
-									node: script,
-									type: "around",
-									method: script.getAttribute(scriptMethodAttribute)
-								});
-								break;
-							case scriptTypeBase + "after":
-								scripts.push({
-									node: script,
-									type: "after",
-									method: script.getAttribute(scriptMethodAttribute)
-								});
-								break;
-							default:
-								scripts.push({
-									node: script,
-									type: "default"
-								});
+						var scriptType = script.getAttribute('type');
+						switch (scriptType) {
+						case scriptTypeBase + 'on':
+							scripts.push({
+								node: script,
+								type: 'on',
+								evt: script.getAttribute(scriptEventAttribute)
+							});
+							break;
+						case scriptTypeBase + 'watch':
+							scripts.push({
+								node: script,
+								type: 'watch',
+								prop: script.getAttribute(scriptPropAttribute)
+							});
+							break;
+						case scriptTypeBase + 'before':
+							scripts.push({
+								node: script,
+								type: 'before',
+								method: script.getAttribute(scriptMethodAttribute)
+							});
+							break;
+						case scriptTypeBase + 'around':
+							scripts.push({
+								node: script,
+								type: 'around',
+								method: script.getAttribute(scriptMethodAttribute)
+							});
+							break;
+						case scriptTypeBase + 'after':
+							scripts.push({
+								node: script,
+								type: 'after',
+								method: script.getAttribute(scriptMethodAttribute)
+							});
+							break;
+						default:
+							scripts.push({
+								node: script,
+								type: 'default'
+							});
 						}
 					});
 				}
@@ -216,36 +221,36 @@ define([
 
 				// Add the instance to the global scope if jsID attribute is set
 				var jsId = obj.node.getAttribute(jsIdAttribute);
-				if(jsId){
+				if (jsId) {
 					lang.setObject(jsId, instance);
 				}
 
 				// Loop through the scripts, post the instantiation of the object and set them up
-				scripts.forEach(function(script){
+				scripts.forEach(function (script) {
 					var fn = functionFromScript(script.node);
-					switch(script.type){
-						case "on":
-							on(instance, script.evt, fn);
-							break;
-						case "watch":
-							instance.watch(script.prop, fn);
-							break;
-						case "before":
-						case "around":
-						case "after":
-							aspect[script.type](instance, script.method, lang.hitch(instance, fn), true);
-							break;
-						default:
-							fn.call(instance);
+					switch (script.type) {
+					case 'on':
+						on(instance, script.evt, fn);
+						break;
+					case 'watch':
+						instance.watch(script.prop, fn);
+						break;
+					case 'before':
+					case 'around':
+					case 'after':
+						aspect[script.type](instance, script.method, lang.hitch(instance, fn), true);
+						break;
+					default:
+						fn.call(instance);
 					}
 				});
 
 				return instance;
 			});
 
-			if(!options.noStart) {
-				instances.forEach(function(instance) {
-					if(typeof instance.startup == "function" && !instance.started) {
+			if (!options.noStart) {
+				instances.forEach(function (instance) {
+					if (typeof instance.startup === 'function' && !instance.started) {
 						instance.startup();
 					}
 				});
@@ -254,7 +259,7 @@ define([
 			return instances;
 		},
 
-		scan: function(rootNode, options) {
+		scan: function (rootNode, options) {
 			// summary:
 			//		Scan the DOM for decorated nodes that should be instantiated as Objects as well as `<script>`
 			//		blocks that indicate modules that should be required.  It returns a promise that is fulfilled with
@@ -274,9 +279,9 @@ define([
 			// an array that may contain declarative require promises
 			var dr = [];
 
-			if(!options.noDeclarativeRequire) {
+			if (!options.noDeclarativeRequire) {
 				// select node that are ``<script type="dojo/require">``
-				query('script[type="dojo/require"]', rootNode).forEach(function(script){
+				query('script[type="dojo/require"]', rootNode).forEach(function (script) {
 					// require in the modules inside the script object
 					dr.push(declarativeRequire(script));
 
@@ -285,12 +290,12 @@ define([
 				});
 			}
 
-			return when(dr.length ? all(dr) : true).then(function(){
+			return when(dr.length ? all(dr) : true).then(function () {
 				var objects = [],
 					mids = [],
 					midHash = {};
 
-				query("[" + typeAttribute + "]", rootNode).forEach(function(node) {
+				query('[' + typeAttribute + ']', rootNode).forEach(function (node) {
 					var mixins = node.getAttribute(mixinsAttribute),
 						type = node.getAttribute(typeAttribute),
 						types = mixins ? [type].concat(mixins.split(/\s*,\s*/)) : [type],
@@ -298,9 +303,9 @@ define([
 
 					try {
 						ctor = getCtor(types);
-					} catch(e) {
-						types.forEach(function(mid) {
-							if(~mid.indexOf("/")){
+					} catch (e) {
+						types.forEach(function (mid) {
+							if (~mid.indexOf('/') && !midHash[mid]) {
 								mids.push(mid);
 								midHash[mid] = true;
 							}
@@ -314,13 +319,13 @@ define([
 					});
 				});
 
-				return when(options.noAutoRequire ? true : promiseRequire(mids)).then(function(){
+				return when(options.noAutoRequire ? true : promiseRequire(mids)).then(function () {
 					return objects;
 				});
 			});
 		},
 
-		parse: function(rootNode, options) {
+		parse: function (rootNode, options) {
 			// summary:
 			//		Scan the DOM for decorated nodes that should be instantiated as Object and instantiate the objects
 			//		and script nodes which indicate modules that should be required into and mapped to global
@@ -340,11 +345,28 @@ define([
 			//
 			// returns: dojo/promise/Promise
 
-			rootNode = rootNode || win.body();
-			options = options || {};
+			// For backwards compatibility with Dijit 1.X at the moment, we need to adjust arguments, because it will
+			// sometimes pass a single argument of options instead of a `parse(null, {})`.
+			if (!options && rootNode && typeof rootNode === 'object') {
+				if ('nodeType' in rootNode) {
+					options = {};
+				} else {
+					options = rootNode;
+					rootNode = options.rootNode || win.body();
+				}
+			} else {
+				rootNode = rootNode || win.body();
+				options = options || {};
+			}
+
+			// This should be the simplified version of the arguments in the future:
+			// rootNode = rootNode || win.body();
+			// options = options || {};
+
+			console.log('parser.parse', rootNode, options);
 
 			var self = this,
-				p = this.scan(rootNode, options).then(function(objects) {
+				p = this.scan(rootNode, options).then(function (objects) {
 					return self.instantiate(objects, options);
 				});
 
